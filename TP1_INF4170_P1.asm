4705718346aff161bbcd5c39aa8fba706324e23f
Emanuel <gonthieremanuel@outlook.com>
2018-11-12 18:03:02 -0500

    Revert "Update TP1_INF4170_P1.asm"
    This reverts commit f5bf0fa5f67cdaa9fb1462764abf7e7bbcb1194f.

--- TP1_INF4170_P1.asm
+++ TP1_INF4170_P1.asm
@@ -8,7 +8,7 @@ main: 	# initialisation du tableau
 	syscall
 	addi 	$v0,$0,5 	# recupérer saisi utilisateur
 	syscall	
-	lui 	$s0,0x1004 	# $s0 adresse de base du tableau NE PAS MODIFIER
+	lui 	$s0,0x1004 	# adresse de base du tableau NE PAS MODIFIER
 	ori 	$s0,$s0,0x0000	# charger adresse tableau
 	add	$s1,$0,$v0	# nb element
 	addi 	$s2,$0,0	# compteur
@@ -47,36 +47,54 @@ done1:	#test de swap
 	
 # void swap(int i, int j) :échange le contenu de deux éléments du tableau avec les indices i et j
 swap:	# enregistrer sur la pile les registre utilisé dans la fonction
-	addi 	$sp, $sp, -4
-	sw 	$s0, 0($sp) 
+	addi 	$sp, $sp, -16
+	sw 	$s1, 12($sp)
+	sw 	$s0, 8($sp) 
+	sw	$t1, 4($sp) 
+	sw 	$t0, 0($sp) 
 	#nb déplacement
 	sll	$t0,$a0,2	# déplacement i	
 	add	$t0,$t0,$s0	# adresse de a[i] = deplacement i + adresse de base du tableau
 	sll	$t1,$a1,2	# déplacement j
 	add	$t1,$t1,$s0	# adresse de a[j] = deplacement j + adresse de base du tableau 
 	#récuperer valeurs
-	lw	$t2,0($t0)	#int temp = a[i]
-	lw 	$t3,0($t1)	#int temp2 = a[j]
+	lw	$s0,0($t0)	#int temp = a[i]
+	lw 	$s1,0($t1)	#int temp2 = a[j]
 	# swap
-	sw	$t3,0($t0)	#a[i] = temp2
-	sw	$t2,0($t1)	#a[j] = temp
+	sw	$s1,0($t0)	#a[i] = temp2
+	sw	$s0,0($t1)	#a[j] = temp
 	# restorer les registres
-	lw 	$s0, 0($sp) 
-	addi 	$sp, $sp, 4
+	lw 	$t0, 0($sp) 
+	lw 	$t1, 4($sp) 
+	lw 	$s0, 8($sp) 
+	lw 	$s1, 12($sp) 
+	addi 	$sp, $sp, 16
 	jr 	$ra
 		
 # int getLeftChildIndex(int index) : retourne l'indice de l'enfant de gauche
-gLci:	addi	$t0,$0,0	# $t0 = 0
+gLci:	# enregistrer sur la pile les registre utilisé dans la fonction
+	addi 	$sp, $sp, -4
+	sw 	$t0, 0($sp)
+	addi	$t0,$0,0	# $t0 = 0
 	sll 	$t0,$a0,1	# index * 2
 	addi	$t0,$t0,1	# index + 1
 	add	$v0,$0,$t0	# return
+	# restorer les registres
+	lw 	$t0, 0($sp)
+	addi 	$sp, $sp, 4 
 	jr	$ra
 
 # int getRightChildIndex(int index) : retourne l'indice de l'enfant de droite
-gRci:	addi	$t0,$0,0	# $t0 = 0
+gRci:	# enregistrer sur la pile les registre utilisé dans la fonction
+	addi 	$sp, $sp, -4
+	sw 	$t0, 0($sp)
+	addi	$t0,$0,0	# $t0 = 0
 	sll 	$t0,$a0,1	# index * 2
 	addi	$t0,$t0,2	# index + 2
 	add	$v0,$0,$t0	# return
+	# restorer les registres
+	lw 	$t0, 0($sp)
+	addi 	$sp, $sp, 4 
 	jr	$ra
 
 

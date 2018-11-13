4a84afb3f93fa12a15b2f1c53ff07e23d8f9f2b0
Emanuel <gonthieremanuel@outlook.com>
2018-11-12 22:27:50 -0500

    Modif restorer les registres pour respecter convention mips

--- TP1_INF4170_P2.asm
+++ TP1_INF4170_P2.asm
@@ -1,5 +1,5 @@
 # Auteurs : Patrick Nolin NOLP18059508
-#           Emanuel Gonthier
+#           Emanuel Gonthier GONE27069202
 .data
 msg_t:		.asciiz "Veuillez entrer le taille de votre tableau (entre 1 et 20): "
 msg_t_err:	.asciiz "\nTaille invalide.\n"
